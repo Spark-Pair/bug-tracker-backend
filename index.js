6e306767c0c3ec0fa58c1c67f8b480bb56c17611
Hassan-Ng <hng39044@gmail.com>
2025-12-15 12:30:44 +0500

    ....

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '500mb' }));
 
 // Firebase Admin
 if (!admin.apps.length) {
